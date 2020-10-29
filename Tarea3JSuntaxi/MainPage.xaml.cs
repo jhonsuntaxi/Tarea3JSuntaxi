bf0eda2d01eace845232950f372aa45bd40e03bd
jhonsuntaxi <73371683+jhonsuntaxi@users.noreply.github.com>
2020-10-29 17:32:34 -0500

    Agregar archivos de proyecto.

---
+++ Tarea3JSuntaxi/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea3JSuntaxi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string usuario, string password)
+        {
+            InitializeComponent();
+
+            lblUser.Text = "Usuario: " + usuario;
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //asignacion de valores desde xaml
+                double dato1 = Convert.ToDouble(txtDato1.Text);
+                double dato2 = Convert.ToDouble(txtDato2.Text);
+
+                double dato3 = Convert.ToDouble(txtDato3.Text);
+                double dato4 = Convert.ToDouble(txtDato4.Text);
+
+                //accion sumar
+
+                double suma = (dato1*0.3) + (dato2*0.2);
+                txtResultado1.Text = suma.ToString();
+
+                double suma2 = (dato3 * 0.3) + (dato4 * 0.2);
+                txtResultado2.Text = suma2.ToString();
+
+                double final = suma+suma2;
+                txtResultadoFinal.Text = final.ToString();
+
+                //Estado
+
+                if (final>=7)
+                {
+                    txtEstado.Text = "APROBADO";
+                }
+                else
+                {
+                    txtEstado.Text = "REPROBADO";
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Mensaje de error", "ERROR" + ex, "ok");
+            }
+
+        }
+    }
+}
---
+++ Tarea3JSuntaxi/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea3JSuntaxi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = txtUsuario.Text;
+            string password = txtPasswd.Text;
+            string user_val = "estudiante2020";
+            string pass_val = "uisrael2020";
+
+
+            try
+            {
+                if (user_val == usuario && pass_val == password)
+                {
+                    await Navigation.PushAsync(new MainPage(usuario,password));
+                }
+                else
+                {
+                    string mensaje = "Usuario o Contraseña incorrectos";
+                    DisplayAlert("Mensaje de Alerta", mensaje, "Aceptar");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Alerta", ex.Message, "Aceptar");
+            }
+
+        }
+    }
+}
